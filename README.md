9e4967944fe4bf9249c04a9782651c55a3ad4ea0
Łukasz Ryś <it212@g2a.com>
2018-10-16 15:14:16 +0200

    Add account history service

--- README.md
+++ README.md
@@ -8,3 +8,5 @@
 * POST **/api/accounts** - create an account
 * POST **/api/accounts/{accountId}/balance/add** - add amount to the account balance
 * POST **/api/accounts/{accountId}/balance/subtract** - subtract amount from the account balance
+
+## Account History Service
---
+++ account-history-service/src/main/java/com/vegesoft/moneytracker/account/history/AccountHistoryApplication.java
@@ -0,0 +1,12 @@
+package com.vegesoft.moneytracker.account.history;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountHistoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountHistoryApplication.class, args);
+    }
+}
